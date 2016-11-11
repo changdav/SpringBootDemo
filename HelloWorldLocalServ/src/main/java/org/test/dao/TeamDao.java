83ebe4f73c78ae233f9625446d3b1549a203c85c
changdav <changdav@grinnell.edu>
2016-11-11 16:31:25 +0200

    Initial commit for Lab1

--- HelloWorldLocalServ/src/main/java/org/test/dao/TeamDao.java
+++ HelloWorldLocalServ/src/main/java/org/test/dao/TeamDao.java
@@ -1,9 +1,11 @@
 package org.test.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.test.domain.Team;
 import java.util.List;
 
+@RestResource(path = "teams", rel = "teams")
 public interface TeamDao extends CrudRepository<Team, Long> {
     List<Team> findAll();
     Team findByName(String name);
